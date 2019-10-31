f0cc42f7b18f003e75194715f2514da37b4027ae
Sam <super8989@gmail.com>
2019-10-30 22:25:32 -0400

    added timer function to re-render every 1 second in componentDidMount

--- src/Timer.js
+++ src/Timer.js
@@ -3,16 +3,20 @@ import React, { Component } from "react";
 class Timer extends Component {
 	constructor(props) {
 		super(props);
+		this.state = { time: new Date() };
 		console.log("In constructor");
 	}
 
 	componentDidMount() {
 		console.log("In component did mount");
+		this.timerID = setInterval(() => {
+			this.setState({ time: new Date() });
+		}, 1000);
 	}
 
 	render() {
 		console.log("In render");
-		return <h1>Timer</h1>;
+		return <h1>{this.state.time.getSeconds()}</h1>;
 	}
 }
 
