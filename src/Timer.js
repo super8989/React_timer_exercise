8a07dea272ebdba0408e153497eef2741f079dfc
Sam <super8989@gmail.com>
2019-10-30 22:16:08 -0400

    lifecycle order

--- src/Timer.js
+++ src/Timer.js
@@ -1,7 +1,17 @@
 import React, { Component } from "react";
 
 class Timer extends Component {
+	constructor(props) {
+		super(props);
+		console.log("In constructor");
+	}
+
+	componentDidMount() {
+		console.log("In component did mount");
+	}
+
 	render() {
+		console.log("In render");
 		return <h1>Timer</h1>;
 	}
 }
